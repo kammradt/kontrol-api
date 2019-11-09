c1bf25df2f0e2087a0d3a4020f3dbe7166e26171
kammradt <vinicius.kammradt1@gmail.com>
2019-11-09 13:40:14 -0300

    :sparkles: Creating userService

---
+++ src/main/java/com/kammradt/learning/service/UserService.java
@@ -0,0 +1,50 @@
+package com.kammradt.learning.service;
+
+import com.kammradt.learning.domain.User;
+import com.kammradt.learning.repository.UserRepository;
+import com.kammradt.learning.service.util.HashUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.kammradt.learning.service.util.HashUtil.generateHash;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user) {
+        String hashedPassword = generateHash(user.getPassword());
+        user.setEmail(hashedPassword);
+
+        return userRepository.save(user);
+    }
+
+    public User update(User user) {
+        String hashedPassword = generateHash(user.getPassword());
+        user.setEmail(hashedPassword);
+
+        return userRepository.save(user);
+    }
+
+    public User findById(Long id) {
+        Optional<User> result = userRepository.findById(id);
+        return result.orElse(null);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User login(String email, String password) {
+        password = generateHash(password);
+        Optional<User> result = userRepository.login(email, password);
+        return result.orElse(null);
+    }
+
+
+}
