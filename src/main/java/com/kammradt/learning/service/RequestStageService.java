55ed64a4895361d9adcde122f4f89265ba9a4489
kammradt <vinicius.kammradt1@gmail.com>
2019-11-09 17:59:38 -0300

    :sparkles: Creating requestStageService

---
+++ src/main/java/com/kammradt/learning/service/RequestStageService.java
@@ -0,0 +1,44 @@
+package com.kammradt.learning.service;
+
+import com.kammradt.learning.domain.RequestStage;
+import com.kammradt.learning.repository.RequestRepository;
+import com.kammradt.learning.repository.RequestStageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class RequestStageService {
+
+    @Autowired
+    private RequestStageRepository requestStageRepository;
+
+    @Autowired
+    private RequestRepository requestRepository;
+
+
+    public RequestStage save(RequestStage requestStage) {
+        requestStage.setRealizationDate(new Date());
+        RequestStage savedRequestStage = requestStageRepository.save(requestStage);
+
+        Long requestBeingUpdated = requestStage.getRequest().getId();
+        requestRepository.updateRequestState(requestBeingUpdated, requestStage.getState());
+
+        return savedRequestStage;
+    }
+
+    public RequestStage findById(Long id) {
+        return requestStageRepository.findById(id).orElse(null);
+    }
+
+    public List<RequestStage> findAllByRequestId(Long requestId) {
+        return requestStageRepository.findAllByRequestId(requestId);
+    }
+
+
+
+
+
+}
