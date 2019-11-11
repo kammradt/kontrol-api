1e9e232b379fc3a8230a2adafc172d39e0f3f066
kammradt <vinicius.kammradt1@gmail.com>
2019-11-11 00:06:19 -0300

    :pencil2: Fixing typo on RequestResource

--- src/main/java/com/kammradt/learning/resource/RequestResource.java
+++ src/main/java/com/kammradt/learning/resource/RequestResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(name = "requests")
+@RequestMapping(value = "requests")
 public class RequestResource {
 
     @Autowired
