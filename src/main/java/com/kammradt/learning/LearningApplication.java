8ce646eb848aa68585eb4924d6b8095260b4eb7a
kammradt <vinicius.kammradt1@gmail.com>
2019-11-24 02:07:08 -0300

    :bug: Fixing config files to be able to deploy

--- src/main/java/com/kammradt/learning/LearningApplication.java
+++ src/main/java/com/kammradt/learning/LearningApplication.java
@@ -2,9 +2,16 @@ package com.kammradt.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearningApplication {
+public class LearningApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LearningApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningApplication.class, args);
