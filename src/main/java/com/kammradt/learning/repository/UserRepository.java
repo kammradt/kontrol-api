cf8c3ba19cc5e4b7d2814abf30db3289c867c56c
kammradt <vinicius.kammradt1@gmail.com>
2019-11-25 08:37:34 -0300

    :sparkles: Adding method to load User by Username (email)

--- src/main/java/com/kammradt/learning/repository/UserRepository.java
+++ src/main/java/com/kammradt/learning/repository/UserRepository.java
@@ -20,4 +20,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying
     @Query("UPDATE User SET role = ?2 WHERE id = ?1")
     int updateRole(Long id, Role role);
+
+    Optional<User> findByEmail(String email);
 }
