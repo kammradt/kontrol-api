491e9a40a4cc2d05393d08eae208eabb6e40ae36
kammradt <vinicius.kammradt1@gmail.com>
2019-11-25 08:40:49 -0300

    :sparkles: Setting up the PasswordEncoder with our custom encoder

---
+++ src/main/java/com/kammradt/learning/security/CustomPasswordEncoder.java
@@ -0,0 +1,19 @@
+package com.kammradt.learning.security;
+
+import com.kammradt.learning.service.util.HashUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomPasswordEncoder implements PasswordEncoder {
+
+    @Override
+    public String encode(CharSequence raw) {
+        return HashUtil.generateHash(raw.toString());
+    }
+
+    @Override
+    public boolean matches(CharSequence raw, String encoded) {
+        return HashUtil.generateHash(raw.toString()).equals(encoded);
+    }
+}
