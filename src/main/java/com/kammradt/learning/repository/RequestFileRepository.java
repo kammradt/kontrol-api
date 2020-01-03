11cd6943f4cd1e221d4f1d120e43b0995bf1a58a
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2020-01-03 09:54:42 -0300

    🗃 Adding @Repository annotation

--- src/main/java/com/kammradt/learning/repository/RequestFileRepository.java
+++ src/main/java/com/kammradt/learning/repository/RequestFileRepository.java
@@ -4,7 +4,9 @@ import com.kammradt.learning.domain.RequestFile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RequestFileRepository extends JpaRepository<RequestFile, Long> {
 
     Page<RequestFile> findAllByRequestId(Long requestId, Pageable pageable);
