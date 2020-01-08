9a3f9ddc2d9f35d54d1c1629ad80e62722637a53
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2020-01-08 11:15:31 -0300

    :sparkles: Adding support to Sort during pagination

--- src/main/java/com/kammradt/learning/model/PageFilterDTO.java
+++ src/main/java/com/kammradt/learning/model/PageFilterDTO.java
@@ -3,14 +3,19 @@ package com.kammradt.learning.model;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Getter @Setter
 public class PageFilterDTO {
 
     private int page = 0;
     private int size = 10;
+    private String sort = "";
 
     public PageFilterDTO(Map<String, String> params) {
         if (params.containsKey("page"))
@@ -18,10 +23,25 @@ public class PageFilterDTO {
 
         if (params.containsKey("size"))
             this.size = Integer.parseInt(params.get("size"));
+
+        if (params.containsKey("sort"))
+            this.sort = params.get("sort") ;
     }
 
     public PageRequest toPageable() {
-        return PageRequest.of(this.page, this.size);
+        List<Sort.Order> orders = Arrays.stream(this.sort.split(","))
+                .filter(prop -> prop.trim().length() > 0)
+                .map(this::mapStringToOrder)
+                .collect(Collectors.toList());
+
+        return PageRequest.of(this.page, this.size, Sort.by(orders));
+    }
+
+    public Sort.Order mapStringToOrder(String column) {
+        column = column.trim();
+        if (column.startsWith("-"))
+            return Sort.Order.desc(column.replace("-", ""));
+        return Sort.Order.asc(column);
     }
 
 }
