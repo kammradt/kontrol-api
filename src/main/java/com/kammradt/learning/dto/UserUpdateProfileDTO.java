aac302f2810bbc71fdb574867b718cfcbf092404
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2020-01-10 03:41:57 -0300

    :bug: Adjusting custom validation on updating User

--- src/main/java/com/kammradt/learning/dto/UserUpdateProfileDTO.java
+++ src/main/java/com/kammradt/learning/dto/UserUpdateProfileDTO.java
@@ -4,15 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 
 @Getter @Setter
 public class UserUpdateProfileDTO {
 
-    @NotEmpty
     private String name;
 
-    @NotEmpty
     @Email(message = "Invalid email")
     private String email;
 
